d5ba062b635c1083144b47b19e0efcfa33fefba9
Samiuddin2000 <94220699+Samiuddin2000@users.noreply.github.com>
2023-06-06 00:54:38 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,54 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC721, Ownable {
+contract DegenToken is ERC20, Ownable {
+
+ 
+    mapping(address => bool) public hasRedeemed;
 
     struct Item {
         string name;
         uint256 price;
     }
 
-    Item[] public gamingItems;
-    mapping(uint256 => address) public itemOwners;
+    Item[] public GamingStore;
 
-    constructor() ERC721("Degen", "DGN") {       
-        gamingItems.push(Item("Rare Sword", 200));
-        gamingItems.push(Item("Epic Armor", 300));
-        gamingItems.push(Item("Legendary Shield", 500));
+    constructor() ERC20("Degen", "DGN") {       
+       GamingStore.push(Item("Rare Sword", 200));
+      GamingStore.push(Item("Epic Armor", 300));
+       GamingStore.push(Item("Legendary Shield", 500));
     }
 
-    function mint(address to, uint256 itemId) public onlyOwner {
-        require(itemId >= 0 && itemId < gamingItems.length, "Invalid item ID");
-        uint256 tokenId = gamingItems.length * 100 + itemId;
-        _mint(to, tokenId);
-        itemOwners[tokenId] = to;
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
-    function transferItem(address to, uint256 tokenId) public {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "Not approved or owner");
-        transferFrom(_msgSender(), to, tokenId);
-        itemOwners[tokenId] = to;
+    function decimals() override public pure returns(uint8){
+        return 0;
     }
 
-    function redeem(uint256 itemId) public {
-        require(itemId >= 0 && itemId < gamingItems.length, "Invalid item ID");
-        uint256 price = gamingItems[itemId].price;
-        require(balanceOf(_msgSender()) >= price, "Insufficient balance");
-
-        uint256 tokenId = gamingItems.length * 100 + itemId;
-        _mint(_msgSender(), tokenId);
-        itemOwners[tokenId] = _msgSender();
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(_msgSender()) >= amount, "Insufficient balance");
+        
+        _transfer(_msgSender(), recipient, amount);
+        return true;
     }
 
-    function burn(uint256 tokenId) public {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "Not approved or owner");
-        _burn(tokenId);
-        delete itemOwners[tokenId];
+function redeem(uint256 itemId) public {
+    require(!hasRedeemed[msg.sender], "Already redeemed");
+
+    Item storage item = GamingStore[itemId];
+    uint256 price = item.price;
+
+    require(balanceOf(msg.sender) >= price, "Insufficient balance");
+
+    hasRedeemed[msg.sender] = true;
+    _burn(msg.sender, price);
+}
+
+
+    function burn(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(_msgSender()) >= amount, "Insufficient balance");
+        
+        _burn(_msgSender(), amount);
     }
- function balanceOf(address account) public view override returns (uint256) {
+
+    function balanceOf(address account) public view override returns (uint256) {
         return super.balanceOf(account);
     }
 }
